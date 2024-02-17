790f21abd18bd275a8e2363f84665beb399379f8
WILLIAN DOUGLAS <67987291+Williands2003@users.noreply.github.com>
2024-02-17 09:42:16 -0300

    Create Tabelo_info.py

---
+++ Tabelo_info.py
@@ -0,0 +1,7 @@
+class TabelaInfo:
+    def _init_(self, nome, id, idade, vulgo, salario):
+        self.nome = nome
+        self.id = id
+        self.idade = idade
+        self.vulgo = vulgo
+        self.salario = salario
